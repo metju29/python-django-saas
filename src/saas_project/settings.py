86f39f45d912c2bd203e212103d5baef7f085941
metju29 <soltysmate@gmail.com>
2025-03-28 18:23:15 +0100

    Updated build args

--- src/saas_project/settings.py
+++ src/saas_project/settings.py
@@ -28,9 +28,6 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # DEBUG = str(os.environ.get("DEBUG_DEBUG")).lower() == "true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-
-print("DEBUG", DEBUG, type(DEBUG))
-
 ALLOWED_HOSTS = [
     ".railway.app", 
 ]
@@ -98,7 +95,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
-DATABASE_URL = config("DATABASE_URL", cast=str)
+DATABASE_URL = config("DATABASE_URL", default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
