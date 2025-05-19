b127f597e34e204e5719412e46438a8aecb598cb
metju29 <soltysmate@gmail.com>
2025-05-19 18:05:38 +0200

    Added example implementation of Django session usage

--- src/saas_project/views.py
+++ src/saas_project/views.py
@@ -31,3 +31,18 @@ def about_view(request, *args, **kwargs):
     html_template = "home.html"
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
+
+VALID_CODE = "abc123"
+
+
+def pw_protected_view(request, *args, **kwargs):
+    is_allowed = request.session.get('protected_page_allowed') or 0
+    print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
+    if request.method == "POST":
+        user_pw_sent = request.POST.get("code") or None
+        if user_pw_sent == VALID_CODE:
+            is_allowed = 1
+            is_allowed = request.session['protected_page_allowed'] = 1
+    if is_allowed:
+        return render(request, "protected/view.html", {})
+    return render(request, "protected/entry.html", {})
