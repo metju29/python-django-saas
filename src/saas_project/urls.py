12555e428044fe0356b47d5cabb111377339ab5b
metju29 <soltysmate@gmail.com>
2026-01-19 18:43:17 +0100

    Added staff only page example

--- src/saas_project/urls.py
+++ src/saas_project/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
-from .views import home_view, about_view, pw_protected_view
+from .views import (
+    home_view, 
+    about_view, 
+    pw_protected_view, 
+    staff_only_view
+    )
 
 
 urlpatterns = [
@@ -27,6 +32,7 @@ urlpatterns = [
     path('about/', about_view),
     path('hello-world/', home_view),
     path('accounts/', include('allauth.urls')),
+    path('protected/staff-only/', staff_only_view),
     path('protected/', pw_protected_view),
     path('admin/', admin.site.urls),
 ]
--- src/saas_project/views.py
+++ src/saas_project/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse
+from math import log
 import pathlib
 from django.shortcuts import render
 from visits.models import PageVisit
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
 
+
+LOGIN_URL = settings.LOGIN_URL
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
@@ -34,10 +38,9 @@ def about_view(request, *args, **kwargs):
 
 VALID_CODE = "abc123"
 
-
 def pw_protected_view(request, *args, **kwargs):
     is_allowed = request.session.get('protected_page_allowed') or 0
-    print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
+    #print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
     if request.method == "POST":
         user_pw_sent = request.POST.get("code") or None
         if user_pw_sent == VALID_CODE:
@@ -46,3 +49,8 @@ def pw_protected_view(request, *args, **kwargs):
     if is_allowed:
         return render(request, "protected/view.html", {})
     return render(request, "protected/entry.html", {})
+
+
+@staff_member_required(login_url=LOGIN_URL)
+def staff_only_view(request, *args, **kwargs):
+    return render(request, "protected/staf-only.html")    
