00da16843befa661fad440d1ab21b4f12dfd478f
metju29 <soltysmate@gmail.com>
2026-04-22 19:40:15 +0200

    Added Sync User Subscription with User Group

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Subscription
+from .models import Subscription, UserSubscription
 
 
 # Register your models here.
 admin.site.register(Subscription)
+admin.site.register(UserSubscription)
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import Group, Permission
+from django.db.models.signals import post_save
+from django.conf import settings
 
+
+User = settings.AUTH_USER_MODEL # auth.User
 
 SUBSCRIPTION_PERMISSIONS = [
             ("advanced", "Advanced Perm"), # subscriptions.advanced
@@ -20,5 +24,28 @@ class Subscription(models.Model):
                           "codename__in": [x[0] for x in SUBSCRIPTION_PERMISSIONS]}
                           )
 
+    def __str__(self):
+        return f"{self.name}"
+
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
+
+
+class UserSubscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, 
+                                     on_delete=models.SET_NULL, 
+                                     null=True, 
+                                     blank=True)
+    active = models.BooleanField(default=True)
+
+
+def user_sub_post_save(sender, instance, *args, **kwargs):
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscription
+    groups = subscription_obj.groups.all()
+    user.groups.set(groups)
+
+
+post_save.connect(user_sub_post_save, sender=UserSubscription)
